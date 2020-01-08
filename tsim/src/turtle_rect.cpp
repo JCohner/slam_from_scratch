76abfe53a06fd0f61dea7c8c4ee7757748ab9773
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-07 19:33:13 -0600

    got test print working

--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -1,6 +1,10 @@
 #include <ros/ros.h>
 int main(int argc, char * argv[]){
+	//initialize node
 	ros::init(argc, argv, "hello_world");
 
+	//create ros node handle
 	ros::NodeHandle nh;
+
+	ROS_INFO("DOINKS");
 }
