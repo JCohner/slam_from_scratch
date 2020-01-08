f9e480334d31a48f395f13bb47d94d12e35c8def
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-08 11:44:42 -0600

    removed top level CMakeLists and got protype of nod efetching from param server

--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+
 int main(int argc, char * argv[]){
 	//initialize node
 	ros::init(argc, argv, "hello_world");
@@ -6,5 +7,8 @@ int main(int argc, char * argv[]){
 	//create ros node handle
 	ros::NodeHandle nh;
 
-	ROS_INFO("DOINKS");
+	std::string hello_world;
+	nh.getParam("/hello_world", hello_world);
+	ROS_INFO(hello_world.c_str());
+	// printf(hello_world);
 }
