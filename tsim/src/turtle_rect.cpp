569b004b41f36fafc43e4140dcf128f5ff4583da
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-09 17:12:54 -0600

    got service server up and running for restarting the circuit

--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -2,8 +2,11 @@
 #include <turtlesim/SetPen.h>
 #include <turtlesim/TeleportAbsolute.h>
 #include <geometry_msgs/Twist.h>
+#include <std_srvs/Empty.h>
 
 #define PI 3.14159
+
+
 
 //structures where parameters from parameter server will be stored
 struct rectangle
@@ -39,53 +42,102 @@ corners corn_;
 ros::ServiceClient setPen_ = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
 ros::ServiceClient teleportAbsolute_ = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
 
-//define publisher to cmd_vel
+//define publisher to; cmd_vel
 ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel",5);
+
+void get_params(void){
+	//get parameters from parameter server
+	nh.getParam("/rectangle/x", rect_.x_);
+	nh.getParam("/rectangle/y", rect_.y_);
+	nh.getParam("/rectangle/width", rect_.width_);
+	nh.getParam("/rectangle/height", rect_.height_);
+	
+	//figure out corners
+	corn_.corner_1[0] = rect_.x_;
+	corn_.corner_1[1] = rect_.y_;
+	corn_.corner_2[0] = rect_.x_ + rect_.width_;
+	corn_.corner_2[1] = rect_.y_;
+	corn_.corner_3[0] = rect_.x_ + rect_.width_;
+	corn_.corner_3[1] = rect_.y_ + rect_.height_;
+	corn_.corner_4[0] = rect_.x_;
+	corn_.corner_4[1] = rect_.y_ + rect_.height_;
+
+	nh.getParam("/robot/trans_vel", robo_.trans_vel_);
+	nh.getParam("/robot/rot_vel", robo_.rot_vel_);
+	nh.getParam("/robot/frequency", robo_.frequency_);
+}
+
+void wait_services(void){
+	//wait for services to become available
+	ros::service::waitForService("/turtle1/set_pen");
+	ros::service::waitForService("/turtle1/teleport_absolute");
+}
+
+void init_telop(){
+	//switch off pen and teleport to lower left corner
+	turtlesim::SetPen pen_req;
+	pen_req.request.off = 1;
+	setPen_.call(pen_req);
+
+	turtlesim::TeleportAbsolute tele_req;
+	tele_req.request.x = rect_.x_;
+	tele_req.request.y = rect_.y_;
+	tele_req.request.theta = 0;
+
+	teleportAbsolute_.call(tele_req);
+
+	//switch on pen
+	pen_req.request.off = 0;
+	setPen_.call(pen_req);
+}
+
 public:
+	
+
 	TurtleRect()
 	{
-		printf("hello wprld\n");
-		//get parameters from parameter server
-		nh.getParam("/rectangle/x", rect_.x_);
-		nh.getParam("/rectangle/y", rect_.y_);
-		nh.getParam("/rectangle/width", rect_.width_);
-		nh.getParam("/rectangle/height", rect_.height_);
+		get_params();
+		wait_services();
+		init_telop();
+	}
+	//declare serviceserver
+	bool reset_callback(std_srvs::Empty::Request& request, std_srvs::Empty::Response& response){
+		printf("request heard\n");
 		
-		//figure out corners
-		corn_.corner_1[0] = rect_.x_;
-		corn_.corner_1[1] = rect_.y_;
-		corn_.corner_2[0] = rect_.x_ + rect_.width_;
-		corn_.corner_2[1] = rect_.y_;
-		corn_.corner_3[0] = rect_.x_ + rect_.width_;
-		corn_.corner_3[1] = rect_.y_ + rect_.height_;
-		corn_.corner_4[0] = rect_.x_;
-		corn_.corner_4[1] = rect_.y_ + rect_.height_;
-
-		nh.getParam("/robot/trans_vel", robo_.trans_vel_);
-		nh.getParam("/robot/rot_vel", robo_.rot_vel_);
-		nh.getParam("/robot/frequency", robo_.frequency_);
-
-		//wait for services to become available
-		ros::service::waitForService("/turtle1/set_pen");
-		ros::service::waitForService("/turtle1/teleport_absolute");
-
-		//switch off pen and teleport to lower left corner
 		turtlesim::SetPen pen_req;
 		pen_req.request.off = 1;
 		setPen_.call(pen_req);
 
+
 		turtlesim::TeleportAbsolute tele_req;
 		tele_req.request.x = rect_.x_;
 		tele_req.request.y = rect_.y_;
-		tele_req.request.theta = 0;
+		this->teleportAbsolute_.call(tele_req);
+		state = 0;
+		i = 0;
 
-		teleportAbsolute_.call(tele_req);
+		//switch on pen
+		pen_req.request.off = 0;
+		setPen_.call(pen_req);
+		return true;
+		
+	}
 
+	void start_service(void){
+		//Define reset service
+		ros::ServiceServer reset_service;
+		reset_service = nh.advertiseService("/traj_reset", &TurtleRect::reset_callback, this);
+		ROS_INFO("Service Started");
+	}
+
+
+	int state = 0;
+	float i = 0;
+	int loop(){
+		ros::ServiceServer reset_service = nh.advertiseService("/traj_reset", &TurtleRect::reset_callback, this);
 		//start on trajectory
 		ros::Rate r(robo_.frequency_);
-		int state = 0;
 		int state_flag = 0;
-		float i = 0;
 		geometry_msgs::Twist vel_req;
 		//command velocity based on state
 		while (ros::ok()){
@@ -134,6 +186,7 @@ public:
 			}
 			vel_pub.publish(vel_req);
 			r.sleep();
+			ros::spinOnce();
 		}
 	}
 
@@ -144,5 +197,7 @@ int main(int argc, char * argv[])
 {
 	ros::init(argc, argv,"hello_world");
 	TurtleRect turtle_rect;
+	turtle_rect.start_service();
+	turtle_rect.loop();
 
 }
