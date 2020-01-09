a9995b27f7954e220122f95a64ea44fc2a186191
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-08 22:45:51 -0600

    node that reads param server and teleports turt to the corner of a rect while also swithcing pen off, next step is to have it traverse da rectangle yo {beans}

--- tsim/src/turtle_rect.cpp
+++ tsim/src/turtle_rect.cpp
@@ -1,14 +1,77 @@
 #include <ros/ros.h>
+#include <turtlesim/SetPen.h>
+#include <turtlesim/TeleportAbsolute.h>
 
-int main(int argc, char * argv[]){
-	//initialize node
-	ros::init(argc, argv, "hello_world");
+//structures where parameters from parameter server will be stored
+struct rectangle
+{
+	int x_;
+	int y_;
+	int width_;
+	int height_;
+};
+struct robot
+{
+	int trans_vel_;
+	int rot_vel_;
+	float frequency_;
+};
 
-	//create ros node handle
-	ros::NodeHandle nh;
+class TurtleRect
+{
+//declatre node handler
+ros::NodeHandle nh;
+//declare parameter structures
+rectangle rect_;
+robot robo_;
+//define service clients
+ros::ServiceClient setPen_ = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
+ros::ServiceClient teleportAbsolute_ = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
 
-	std::string hello_world;
-	nh.getParam("/hello_world", hello_world);
-	ROS_INFO(hello_world.c_str());
-	// printf(hello_world);
+public:
+	TurtleRect()
+	{
+		//get parameters from parameter server
+		nh.getParam("/rectangle/x", rect_.x_);
+		nh.getParam("/rectangle/y", rect_.y_);
+		nh.getParam("/rectangle/width", rect_.width_);
+		nh.getParam("/rectangle/height", rect_.height_);
+		nh.getParam("/robot/trans_vel", robo_.trans_vel_);
+		nh.getParam("/robot/rot_vel", robo_.rot_vel_);
+		nh.getParam("/robot/frequency", robo_.frequency_);
+
+		//wait for services to become available
+		ros::service::waitForService("/turtle1/set_pen");
+		ros::service::waitForService("/turtle1/teleport_absolute");
+
+		//switch off pen and teleport to lower left corner
+		turtlesim::SetPen pen_req;
+		pen_req.request.off = 1;
+		setPen_.call(pen_req);
+
+		turtlesim::TeleportAbsolute tele_req;
+		tele_req.request.x = rect_.x_;
+		tele_req.request.y = rect_.y_;
+		tele_req.request.theta = 0;
+
+		teleportAbsolute_.call(tele_req);
+
+		//start on trajectory
+		ros::Rate r(robo_.frequency_);
+		int state = 0;
+		while (ros::ok()){
+
+
+			r.sleep();
+		}
+	}
+
+};
+
+
+int main(int argc, char * argv[])
+{
+	ros::init(argc, argv,"hello_world");
+	TurtleRect turtle_rect;
+
 }
