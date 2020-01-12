7e65373b697f8abdce45d92f31d883c4baf68f69
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-11 18:19:59 -0600

    overloaded stream output operators operating on Vector2D types (wammy)

--- rigid2d/rigid2d.cpp
+++ rigid2d/rigid2d.cpp
@@ -2,22 +2,37 @@
 
 namespace rigid2d
 {
+	//write constructor
 	Transform2D::Transform2D()
 	{
 
 	}
 
-	std::ostream & operator<<(std::ostream & os, const Transform2D & tf){
-
-
+	std::ostream & operator<<(std::ostream & os, const Vector2D & v)
+	{
+		std::cout << "x: " << v.x << "\n";
+		std::cout << "y: " << v.y << "\n";
 		return os;
 	}
 
-	std::istream & operator>>(std::istream & is, Transform2D & tf){
-
-
+	std::istream & operator>>(std::istream & is, Vector2D & v)
+	{
+		std::cin >> v.x;
+		std::cin >> v.y;
 		return is;
-	}
+	}	
+
+	// std::ostream & operator<<(std::ostream & os, const Transform2D & tf)
+	// {
+
+	// 	return os;
+	// }
+
+	// std::istream & operator>>(std::istream & is, Transform2D & tf)
+	// {
+
+	// 	return is;
+	// }
 
 
 }
