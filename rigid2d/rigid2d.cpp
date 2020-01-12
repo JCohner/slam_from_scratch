621e02f80ba66258828f84795223cbc8f5b561c3
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-12 17:38:14 -0600

    defined * operator as friend function for matrix transformation multiplication

--- rigid2d/rigid2d.cpp
+++ rigid2d/rigid2d.cpp
@@ -39,6 +39,15 @@ namespace rigid2d
 		y = trans.y;
 	}
 
+	Transform2D Transform2D::inv() const {
+		Transform2D inv;
+		inv.x = -cos(deg2rad(this->theta)) * this->x - sin(deg2rad(this->theta)) * this->y;
+		inv.y = sin(deg2rad(this->theta)) * this->x - cos(deg2rad(this->theta)) * this->y;
+		inv.theta = -(this->theta);
+		inv.ctheta = this->ctheta;
+		inv.stheta = -(this->stheta);
+		return inv;
+	}
 
 	//outstream op overload for vectors
 	std::ostream & operator<<(std::ostream & os, const Vector2D & v)
@@ -78,15 +87,17 @@ namespace rigid2d
 		return is;
 	}
 
-	Transform2D Transform2D::inv() const {
-		Transform2D inv;
-		inv.x = -cos(deg2rad(this->theta)) * this->x - sin(deg2rad(this->theta)) * this->y;
-		inv.y = sin(deg2rad(this->theta)) * this->x - cos(deg2rad(this->theta)) * this->y;
-		inv.theta = -(this->theta);
-		inv.ctheta = this->ctheta;
-		inv.stheta = -(this->stheta);
-		return inv;
+	Transform2D operator*(Transform2D lhs, const Transform2D & rhs){
+		Transform2D prod; 
+		prod.theta = lhs.theta + rhs.theta;
+		prod.ctheta = cos(prod.theta);
+		prod.stheta = sin(prod.theta);
+		prod.x = cos(lhs.theta) * rhs.x - sin(lhs.theta) * rhs.y + lhs.x;
+		prod.y = cos(lhs.theta) * rhs.y + sin(lhs.theta) * rhs.x + lhs.y; 
+
+		return prod;
 	}
 
+	
 
 }
