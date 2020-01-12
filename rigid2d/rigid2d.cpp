ab93c5994b3d31b7fbe619e250299583a73eb9c6
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-12 17:15:57 -0600

    defined overloaded constructors, refined transform input stream and inverse transform to accept degree inputs

--- rigid2d/rigid2d.cpp
+++ rigid2d/rigid2d.cpp
@@ -12,6 +12,34 @@ namespace rigid2d
 		y = 0;
 	}
 
+	//pure translation constructor
+	Transform2D::Transform2D(const Vector2D & trans){
+		theta = 0;
+		ctheta = 0;
+		stheta = 0;
+		x = trans.x;
+		y = trans.y;
+	}
+
+	//pure rotation constructor
+	Transform2D::Transform2D(double radians){
+		theta = rad2deg(radians);
+		ctheta = cos(radians);
+		stheta = sin(radians);
+		x = 0;
+		y = 0;
+	}
+
+	//combo constructor
+	Transform2D::Transform2D(const Vector2D & trans, double radians){
+		theta = rad2deg(radians);
+		ctheta = cos(radians);
+		stheta = sin(radians);
+		x = trans.x;
+		y = trans.y;
+	}
+
+
 	//outstream op overload for vectors
 	std::ostream & operator<<(std::ostream & os, const Vector2D & v)
 	{
@@ -31,7 +59,7 @@ namespace rigid2d
 	//outstream op overload for transforms2ds
 	std::ostream & operator<<(std::ostream & os, const Transform2D & tf)
 	{
-		std::cout << "ctheta: " << tf.ctheta << "\n" << "stheta: " << tf.stheta << "\n"<< "x: " << tf.x << " \n" << "y: " << tf.y << "\n";
+		std::cout << "theta: " << tf.theta << "\t" <<"ctheta: " << tf.ctheta << "\t" << "stheta: " << tf.stheta << "\n"<< "x: " << tf.x << " \t" << "y: " << tf.y << "\n";
 		return os;
 	}
 
@@ -42,15 +70,19 @@ namespace rigid2d
 		std::cin >> tf.x;
 		std::cin >> tf.y;
 		//use as an opprtunity to fill out fields defined by input
-		tf.ctheta = cos(tf.theta);
-		tf.stheta = sin(tf.theta);
+		float x = cos(deg2rad(tf.theta));
+		float y = sin(deg2rad(tf.theta));
+		(almost_equal(x, 0.0)) ? (tf.ctheta = 0) : (tf.ctheta =  x);
+		(almost_equal(y, 0.0)) ? (tf.stheta = 0) : (tf.stheta =  y);
+
 		return is;
 	}
 
 	Transform2D Transform2D::inv() const {
 		Transform2D inv;
-		inv.x = -cos(this->theta) * this->x - sin(this->theta) * this->y;
-		inv.y = sin(this->theta) * this->x - cos(this->theta) * this->y;
+		inv.x = -cos(deg2rad(this->theta)) * this->x - sin(deg2rad(this->theta)) * this->y;
+		inv.y = sin(deg2rad(this->theta)) * this->x - cos(deg2rad(this->theta)) * this->y;
+		inv.theta = -(this->theta);
 		inv.ctheta = this->ctheta;
 		inv.stheta = -(this->stheta);
 		return inv;
