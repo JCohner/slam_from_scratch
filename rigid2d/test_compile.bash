07c7bb981d35fab568782ff2e1dfd9c359e38723
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-13 11:46:33 -0600

    testing bash script

--- rigid2d/test_compile.bash
+++ rigid2d/test_compile.bash
@@ -1,6 +1,6 @@
 g++ -Wall -Wextra -g -std=c++17 -o rigid2d_test main.cpp rigid2d.cpp
-if cmp -s ./rigid2d_test < test1_input.txt test1_answer.txt; then
-	echo Failure!
-else 
-	echo Success!
-fi
+./rigid2d_test < test1_input.txt > output.txt
+# cat output.txt
+# cat test1_answer.txt
+cmp -s output.txt test1_answer.txt && echo "Success!"
+cmp -s output.txt test1_answer.txt || echo "Failure!" #maurice helped me with this
