6b5e2dff736af1561155e6466db9810cf1395637
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-16 17:22:56 -0600

    added tests of constant expressions

--- rigid2d/test/rigid2d/rigid2d_test.cpp
+++ rigid2d/test/rigid2d/rigid2d_test.cpp
@@ -1,10 +1,36 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
-TEST(TestSuite1, testCase1)
+#include "rigid2d/rigid2d.hpp"
+
+namespace rigid2d {
+//Test our constant expressions
+TEST(ConstExpr, deg2rad)
 {
-    ASSERT_EQ(2, 3);
-    // There are many other ASSERT (fatal) and EXPECT (non-fatal, tests continue)
-    // macros in google test
-    // When first setting up tests, I like to write one that fails,
-    // to ensure it is being run at the appropriate time
+    ASSERT_DOUBLE_EQ(deg2rad(180.0), PI);
+    ASSERT_DOUBLE_EQ(deg2rad(90.0), PI/2);
+    ASSERT_DOUBLE_EQ(deg2rad(45), PI/4);   
+
+}
+TEST(ConstExpr, rad2deg)
+{
+    ASSERT_DOUBLE_EQ(rad2deg(PI), 180.0);
+    ASSERT_DOUBLE_EQ(rad2deg(2*PI), 360);
+    ASSERT_DOUBLE_EQ(rad2deg(PI/4), 45);
+    ASSERT_EQ(almost_equal(rad2deg(PI/3), 60), true);  
+}
+TEST(ConstExpr, almost_equal)
+{
+    ASSERT_EQ(almost_equal(0, 0), true);
+    ASSERT_EQ(almost_equal(0.001, 0.005, 1.0e-2), true);
+    ASSERT_EQ(almost_equal(0.001, 0.005, 1.0e-12), false);
+}
+
+// //test our 2D vectors
+// TEST(Vector2D, OutOpOverload){
+// 	Vector2D vec;
+// 	vec.x = 1;
+// 	vec.y = 2;
+// 	std::ostream os = std::cout << vec;
+// }
+
 }
