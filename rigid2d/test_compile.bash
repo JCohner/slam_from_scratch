d7638b66a42cfbbc005d27ad7313d49b46c89335
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-16 12:55:50 -0600

    removed testing files from previous iteration

--- rigid2d/test_compile.bash
+++
@@ -1,6 +0,0 @@
-g++ -Wall -Wextra -g -std=c++17 -o rigid2d_test main.cpp rigid2d.cpp
-./rigid2d_test < test1_input.txt > output.txt
-# cat output.txt
-# cat test1_answer.txt
-cmp -s output.txt test1_answer.txt && echo "Success!"
-cmp -s output.txt test1_answer.txt || echo "Failure!" #maurice helped me with this
