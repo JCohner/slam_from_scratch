3b1467bde2cc475bf7ad9d26dc4fe5b5a550c1ec
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-27 12:21:18 -0600

    placeholder for testing Waypoints class

---
+++ rigid2d/test/waypoints/waypoints_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <ros/ros.h>
+#include "rigid2d/rigid2d.hpp"
+#include "diff_drive/diff_drive.hpp"
+#include <iostream>
+#include <sstream>
+
+namespace rigid2d{
+	
+}
