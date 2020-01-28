f2176eced31d64e059e034f2c7c32668650b094b
JCohner <joshuacohen2019@u.northwestern.edu>
2020-01-27 21:50:21 -0600

    beginings of this node

---
+++ tsim/src/turtle_way.cpp
@@ -0,0 +1,11 @@
+#include <ros/ros.h>
+#include <turtlesim/SetPen.h>
+#include <turtlesim/TeleportAbsolute.h>
+#include <geometry_msgs/Twist.h>
+#include <std_srvs/Empty.h>
+#include <tsim/PoseError.h>
+
+int main(int argc, char * argv[]){
+	ros::init(argc, argv,"turtle_way");
+
+}
