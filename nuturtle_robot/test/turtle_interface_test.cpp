6ee8ced7efba39a99885084db72717560e4356bf
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-08 20:18:28 -0600

    fleshing out testing framework, will work on actual implementation now

--- nuturtle_robot/test/turtle_interface_test.cpp
+++ nuturtle_robot/test/turtle_interface_test.cpp
@@ -1,11 +1,36 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <nuturtlebot/WheelCommands.h>
+#include "rigid2d/rigid2d.hpp"
+#include "diff_drive/diff_drive.hpp"
+
+TEST(cmd_vel, no_rot)
+{
+
+}
+
+ros::Publisher	vel_pub;
+ros::Subscriber wheel_cmd_sub; 
+
+void wheel_cmd_sub_callback(nuturtlebot::WheelCommands data)
+{
+	;
+}
+
+void setup()
+{
+	ros::NodeHandle nh;
+	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	wheel_cmd_sub = nh.subscribe("wheel_cmd", 1, &wheel_cmd_sub_callback);
+}
+
 
 int main(int argc, char * argv[])
 {
 	testing::InitGoogleTest(&argc, argv);
     ros::init(argc, argv, "test_node_name");
-    ros::NodeHandle nh;
+    setup();
     return RUN_ALL_TESTS();
 }
 
