772fd8704daebe0d793acf91b585c3d31a0b718e
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-08 22:06:50 -0600

    finished requested test implementation

--- nuturtle_robot/test/turtle_interface_test.cpp
+++ nuturtle_robot/test/turtle_interface_test.cpp
@@ -2,13 +2,19 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <nuturtlebot/WheelCommands.h>
+#include <nuturtlebot/SensorData.h>
+#include <sensor_msgs/JointState.h>
 #include "rigid2d/rigid2d.hpp"
 #include "diff_drive/diff_drive.hpp"
 
 ros::Publisher	vel_pub;
 ros::Subscriber wheel_cmd_sub; 
-bool x = true;
+ros::Publisher	sensor_pub;
+ros::Subscriber js_sub; 
+bool x;
+bool y;
 rigid2d::WheelVelocities wheel_vels;
+sensor_msgs::JointState js;
 
 TEST(cmd_vel, no_rot)
 {
@@ -60,6 +66,32 @@ TEST(cmd_vel, trans_and_rot)
 	ASSERT_EQ(wheel_vels.right, 44);	
 }
 
+TEST(sens_sub, encoder_counts)
+{
+	nuturtlebot::SensorData msg;
+	msg.left_encoder = 50; 
+	msg.right_encoder = 60;
+	sensor_pub.publish(msg);
+	y = true; 
+	ros::Rate r(60);
+	while(y)
+	{
+		ros::spinOnce();
+		r.sleep();
+	}
+	ASSERT_EQ(js.position[0], 50);
+	ASSERT_EQ(js.position[1], 60);
+	ASSERT_EQ(js.velocity[0], 50);
+	ASSERT_EQ(js.velocity[1], 60);
+}
+
+void js_sub_callback(sensor_msgs::JointState data)
+{
+	y = false;
+	js = data;
+	return;
+}
+
 void wheel_cmd_sub_callback(nuturtlebot::WheelCommands data)
 {
 	x = false;
@@ -74,6 +106,8 @@ void setup()
 	ros::NodeHandle nh;
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1, true);
 	wheel_cmd_sub = nh.subscribe("wheel_cmd", 1, &wheel_cmd_sub_callback);
+	sensor_pub = nh.advertise<nuturtlebot::SensorData>("sensor_data", 1,true);
+	js_sub = nh.subscribe("joint_states", 1, &js_sub_callback);
 }
 
 int main(int argc, char * argv[])
