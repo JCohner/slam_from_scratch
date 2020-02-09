f05ff08e5961e6a779c3835d50bee6409f036522
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-08 21:06:51 -0600

    successful initial testing of cmd_vel to wheel vel with no rot

--- nuturtle_robot/test/turtle_interface_test.cpp
+++ nuturtle_robot/test/turtle_interface_test.cpp
@@ -5,26 +5,43 @@
 #include "rigid2d/rigid2d.hpp"
 #include "diff_drive/diff_drive.hpp"
 
-TEST(cmd_vel, no_rot)
-{
-
-}
-
 ros::Publisher	vel_pub;
 ros::Subscriber wheel_cmd_sub; 
+bool x = true;
+rigid2d::WheelVelocities wheel_vels;
+
+TEST(cmd_vel, no_rot)
+{
+	geometry_msgs::Twist msg;
+	msg.angular.z = 0;
+	msg.linear.x = 0.02;
+	vel_pub.publish(msg);
+	ros::Rate r(60);
+	while(x)
+	{
+		r.sleep();
+		ros::spinOnce();
+	}
+
+	ASSERT_EQ(wheel_vels.left, wheel_vels.right);
+	// ASSERT_EQ(1,1);
+}
 
 void wheel_cmd_sub_callback(nuturtlebot::WheelCommands data)
 {
-	;
+	x = false;
+	wheel_vels.left = data.left_velocity;
+	wheel_vels.right = data.right_velocity;
+
+	return;
 }
 
 void setup()
 {
 	ros::NodeHandle nh;
-	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
+	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1, true);
 	wheel_cmd_sub = nh.subscribe("wheel_cmd", 1, &wheel_cmd_sub_callback);
 }
-
 
 int main(int argc, char * argv[])
 {
