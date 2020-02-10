a830f3f27598b35a2be14942b1f904f662229fe2
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-10 13:00:02 -0600

    added mainloop fixed vel pub

--- nuturtle_robot/src/rotation.cpp
+++ nuturtle_robot/src/rotation.cpp
@@ -1,11 +1,14 @@
 #include <ros/ros.h>
 #include "nuturtle_robot/Start.h"
 #include <turtlesim/TeleportAbsolute.h>
+#include <geometry_msgs/Twist.h>
 
 /*Global Variables*/
 ros::ServiceServer start_srv;
 ros::ServiceClient set_pose;
+ros::Publisher vel_pub;
 bool set_pose_available;
+double freq = 0.25; //TODO:make this a command line or param server parameter
 
 bool start_srv_callback(nuturtle_robot::Start::Request& req, nuturtle_robot::Start::Response& resp)
 {
@@ -19,6 +22,7 @@ void setup()
 {
 	ros::NodeHandle nh;
 	start_srv = nh.advertiseService("start", &start_srv_callback);
+	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 	if (ros::service::waitForService("set_pose", 1000)){
 		set_pose_available = true;
 		set_pose = nh.serviceClient<turtlesim::TeleportAbsolute>("set_pose");
@@ -34,10 +38,20 @@ void setup()
 	}
 }
 
+void loop()
+{
+	ros::Rate r(60);
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		r.sleep();
+	}
+}
+
 int main(int argc, char * argv[])
 {
 	ros::init(argc, argv, "rotation");
 	setup();
-
+	loop();
 	return 0;
 }
