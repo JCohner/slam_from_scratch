b1d11ea47f73cff92bae46a6bd2f46d37fa05bcb
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-11 20:27:09 -0600

    got it rotating!

--- nuturtle_robot/src/rotation.cpp
+++ nuturtle_robot/src/rotation.cpp
@@ -10,21 +10,25 @@ static ros::ServiceClient fake_set_pose;
 static ros::ServiceClient set_pose;
 static ros::Publisher vel_pub;
 static bool set_pose_available;
-static int state;
+// static int state;
 static int start = 0; //0 -> rotation, 1 paused
 static int num_rots = 0; 
 static double max_rot_vel = 2.84;
 static double frac_vel = 0.25; //TODO:make this a command line or param server parameter //fraction of max velocity
 static double rot_speed = frac_vel * max_rot_vel; //TODO: will have to move this to setup 
-static ros::Timer rot_timer
-static ros::Timer pause_timer;
-
+static ros::Timer rot_timer;
+static double freq;
+static double rotation = 2 * rigid2d::PI;
+static double rot_periodT = rotation/rot_speed;
+static double counts_per_rev;
+double count = 0;
+// static ros::Timer pause_timer;
 
 bool start_srv_callback(nuturtle_robot::Start::Request& req, nuturtle_robot::Start::Response& resp)
 {
 	//0 indicates CCW; 1 indicates CW traversal
 	start = 1;
-	state = 0;
+	num_rots = 0;
 	if (req.direction == 1)
 	{
 		rot_speed = -rot_speed;
@@ -43,44 +47,62 @@ bool start_srv_callback(nuturtle_robot::Start::Request& req, nuturtle_robot::Sta
 
 void rot_timerCallback(const ros::TimerEvent& ev)
 {
-	ROS_INFO("ROTATION triggering");
-	if (!state && start)
-	{
-		// state = !state; //set state to true
-		geometry_msgs::Twist msg;
-		msg.angular.z = rot_speed;
-		vel_pub.publish(msg);
+	if (start && (num_rots < 20))
+	{	
+		if(count < counts_per_rev){
+			ROS_INFO("ROTATION triggering");
+			geometry_msgs::Twist msg;
+			msg.angular.z = rot_speed;
+			vel_pub.publish(msg);
+		} 
+		else if (count <= (1.05 * counts_per_rev)){
+			ROS_INFO("Pause triggering");
+			geometry_msgs::Twist msg;
+			vel_pub.publish(msg);
+		} 
+		else {
+			ROS_INFO("ONE ROT DONE triggering");
+			count = 0;
+			num_rots++;
+		} 
+		++count;
 	}
 }
 
-void pause_timerCallback(const ros::TimerEvent& ev)
-{
-	if (state && start)
-	{
-		ROS_INFO("PAUSE triggering");
-		state = !state; //set state to false
-		geometry_msgs::Twist msg;
-		vel_pub.publish(msg);
-		num_rots++;
-	}
+// void pause_timerCallback(const ros::TimerEvent& ev)
+// {
+// 	if (state && start)
+// 	{
+// 		ROS_INFO("PAUSE triggering");
+// 		state = !state; //set state to false
+// 		geometry_msgs::Twist msg;
+// 		vel_pub.publish(msg);
+// 		num_rots++;
+// 	}
 
-	if (start > 21)
-	{
-		start = 0;
-	}
-}
+// 	if (start > 21)
+// 	{
+// 		start = 0;
+// 	}
+// }
 
 void setup()
 {
 	ros::NodeHandle nh;
-	double rotation = 2 * rigid2d::PI;
-	double rot_periodT = rotation/rot_speed;
+	/*get freq*/
+	nh.getParam("/freq", freq);
 	ROS_INFO("ROTATION: %f", rot_periodT);
+	rot_timer = nh.createTimer(ros::Duration(1.0/freq), rot_timerCallback);
 	// pause_timer = nh.createTimer(ros::Duration(rot_periodT/20.0), pause_timerCallback);
-	rot_timer = nh.createTimer(ros::Duration(rot_periodT), rot_timerCallback);
 	// nh.getParam("velocity/max_rot", max_rot_vel);
+	
+	counts_per_rev = rot_periodT * freq;
+	ROS_INFO("num counts per rev: %f", counts_per_rev);
+	/*Initialize services and publishers*/
 	start_srv = nh.advertiseService("start", &start_srv_callback);
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+	
+	/*Check if set pose service is available*/
 	bool real_pose = ros::service::waitForService("set_pose");
 	bool fake_pose = ros::service::waitForService("fake/set_pose");
 	if (real_pose && fake_pose){
