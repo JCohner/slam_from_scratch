10c6bb993bd4c1b3f6cf2dbf9ad12d7f5189ff91
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-17 18:53:02 -0600

    made a pub marker function that publishes all markers to rviz

--- nuturtle_robot/src/real_waypoint.cpp
+++ nuturtle_robot/src/real_waypoint.cpp
@@ -2,8 +2,10 @@
 #include "waypoints/waypoints.hpp"
 #include "rigid2d/rigid2d.hpp"
 #include "diff_drive/diff_drive.hpp"
+#include <visualization_msgs/Marker.h>
 
 //grab these from param server eventually
+/*robot & general properties*/
 static double wheel_base;
 static double wheel_radius;
 static double encoder_ticks_per_rev;
@@ -14,6 +16,7 @@ static double freq;
 static double frac_vel;
 static double wheel_command_max;
 
+/*waypoint structures*/
 rigid2d::Waypoints waypoints;
 std::vector<double> curr_waypoint(3,0);
 std::vector<double> x(5,0);
@@ -21,11 +24,57 @@ std::vector<double> y(5,0);
 std::vector<double> th(5,0);
 unsigned int i; 
 
+/*marker pub*/
+ros::Publisher marker_pub;
+
+
+void pub_marker()
+{
+	uint32_t shape = visualization_msgs::Marker::CYLINDER;
+	std::vector<visualization_msgs::Marker> marks(5);
+	std::vector<std::string> frame_names = {"/marker0", "/marker1", "/marker2", "/marker3", "/marker4"}; 
+	int i = 0;
+	ros::Rate r(60);
+	for (auto mark : marks)
+	{
+		curr_waypoint = waypoints.get_waypoint_at(i);
+		mark.header.frame_id = frame_names.at(i);
+		mark.header.stamp = ros::Time::now();
+		mark.ns = "mork";
+		mark.id = i;
+		mark.type = shape;
+		mark.action = visualization_msgs::Marker::ADD;
+		mark.pose.position.x = curr_waypoint.at(0);
+		mark.pose.position.y = curr_waypoint.at(1);
+		mark.pose.position.z = 0;
+		mark.pose.orientation.x = 0.0;
+		mark.pose.orientation.y = 0.0;
+		mark.pose.orientation.z = 0.0;
+		mark.pose.orientation.w = 1.0;
+
+		// Set the scale of the marker -- 1x1x1 here means 1m on a side
+		mark.scale.x = 1.0;
+		mark.scale.y = 1.0;
+		mark.scale.z = 1.0;
+
+		// Set the color -- be sure to set alpha to something non-zero!
+		mark.color.r = 0.0f;
+		mark.color.g = 1.0f;
+		mark.color.b = 0.0f;
+		mark.color.a = 1.0;
+		mark.lifetime = ros::Duration();
+		marker_pub.publish(mark);
+		i++;
+		r.sleep();
+	}
+}
+
 void setup()
 {
 	ros::NodeHandle nh;
 	ros::NodeHandle nh_priv("~");
-	//get robot properties
+	/*Get from param*/
+	//robot props
 	nh.getParam("/wheel/radius", wheel_radius);
 	nh.getParam("/wheel/base", wheel_base);
 	nh.getParam("/wheel/encoder_ticks_per_rev", encoder_ticks_per_rev);
@@ -34,7 +83,6 @@ void setup()
 	nh.getParam("/motor/no_load_speed", max_motor_speed);
 	nh.getParam("/motor/max_power", wheel_command_max);
 	nh_priv.getParam("frac_vel",frac_vel);
-
 	//get waypoint properties
 	nh.getParam("/waypoint_x", x);
 	nh.getParam("/waypoint_y", y);
@@ -46,6 +94,9 @@ void setup()
 		waypoints.addWaypoint(std::vector<double> {x[i], y[i], th[i]}, i);
 	}
 	curr_waypoint = waypoints.get_curr_waypoint();
+
+	/*Node stuff*/
+	marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 }
 
 int main(int argc, char * argv[])
