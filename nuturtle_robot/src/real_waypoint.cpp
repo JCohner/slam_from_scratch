4943a6b45af60bf0785a0a565037edb5b2e540c5
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-17 23:21:57 -0600

    now stops after 1 circuit, publishes markers right

--- nuturtle_robot/src/real_waypoint.cpp
+++ nuturtle_robot/src/real_waypoint.cpp
@@ -26,7 +26,7 @@ std::vector<double> curr_waypoint(3,0);
 std::vector<double> x(5,0);
 std::vector<double> y(5,0);
 std::vector<double> th(5,0);
-unsigned int i; 
+unsigned int counter; 
 
 /*marker pub*/
 ros::Publisher marker_pub;
@@ -40,6 +40,10 @@ static ros::ServiceClient set_pose;
 static int start = 0; 
 static ros::Timer timer;
 geometry_msgs::Twist cmd_twist;
+geometry_msgs::Twist prev_cmd_twist;
+
+//forward decls
+void pub_marker();
 
 geometry_msgs::Twist VbToRos(rigid2d::Twist2D Vb){
 	geometry_msgs::Twist twist;
@@ -54,14 +58,25 @@ void timerCallback(const ros::TimerEvent& ev)
 	if (start)
 	{
 		cmd_twist = VbToRos(waypoints.nextWaypoint());
+		if (cmd_twist.angular.z != prev_cmd_twist.angular.z) {
+			++counter;
+			prev_cmd_twist = cmd_twist;
+			if (counter > 9){
+				start = 0;
+				return;
+			}
+		}
+
 		vel_pub.publish(cmd_twist);
-		ros::spinOnce();
 	}
+	return;
 }
 
 bool start_srv_callback(nuturtle_robot::Start::Request& req, nuturtle_robot::Start::Response& resp)
 {
 	start = 1;
+	counter = 0;
+	pub_marker();
 	turtlesim::TeleportAbsolute pose;
 	pose.request.theta = 0;
 	pose.request.x = 0;
@@ -81,20 +96,20 @@ void pub_marker()
 {
 	uint32_t shape = visualization_msgs::Marker::CYLINDER;
 	std::vector<visualization_msgs::Marker> marks(5);
-	std::vector<std::string> frame_names = {"/marker0", "/marker1", "/marker2", "/marker3", "/marker4"}; 
+	// std::vector<std::string> frame_names = {"marker0", "marker1", "marker2", "marker3", "marker4"}; 
 	int i = 0;
 	ros::Rate r(60);
 	for (auto mark : marks)
 	{
 		curr_waypoint = waypoints.get_waypoint_at(i);
-		mark.header.frame_id = frame_names.at(i);
+		mark.header.frame_id = "odom";
 		mark.header.stamp = ros::Time::now();
 		mark.ns = "mork";
 		mark.id = i;
 		mark.type = shape;
 		mark.action = visualization_msgs::Marker::ADD;
-		mark.pose.position.x = curr_waypoint.at(0);
-		mark.pose.position.y = curr_waypoint.at(1);
+		mark.pose.position.x = curr_waypoint.at(1);
+		mark.pose.position.y = curr_waypoint.at(0);
 		mark.pose.position.z = 0;
 		mark.pose.orientation.x = 0.0;
 		mark.pose.orientation.y = 0.0;
@@ -103,9 +118,9 @@ void pub_marker()
 
 		// Set the scale of the marker -- 1x1x1 here means 1m on a side
 		//TODO: scale these properly
-		mark.scale.x = 1.0;
-		mark.scale.y = 1.0;
-		mark.scale.z = 1.0;
+		mark.scale.x = 0.2;
+		mark.scale.y = 0.2;
+		mark.scale.z = 0.2;
 
 		// Set the color -- be sure to set alpha to something non-zero!
 		mark.color.r = 0.0f;
@@ -139,14 +154,15 @@ void setup()
 	nh.getParam("/waypoint_th", th);
 	nh.getParam("/freq", freq);
 
-	waypoints = rigid2d::Waypoints(rigid2d::Transform2D(rigid2d::Vector2D(x[0],y[0]), th[0]), frac_vel* max_rot_vel, frac_vel* max_trans_vel, freq);
+	waypoints = rigid2d::Waypoints(rigid2d::Transform2D(rigid2d::Vector2D(x[0],y[0]), th[0]),
+									frac_vel* max_rot_vel, frac_vel* max_trans_vel, freq);
 	for (unsigned int i = 0; i < x.size(); i++){
 		waypoints.addWaypoint(std::vector<double> {x[i], y[i], th[i]}, i);
 	}
 	curr_waypoint = waypoints.get_curr_waypoint();
 
 	/*Node stuff*/
-	marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+	marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
 	vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
 	start_srv = nh.advertiseService("start", &start_srv_callback);
 	stop_srv = nh.advertiseService("stop", &stop_srv_callback);
@@ -161,6 +177,7 @@ int main(int argc, char * argv[])
 {
 	ros::init(argc, argv,"real_waypoint");
 	setup();
-	// loop();
+	waypoints.start();
+	ros::spin();
 	return 0;
 }
