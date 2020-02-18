3667939a82bdf25ebc4b06e32b546933e606822a
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-17 23:22:53 -0600

    made constructor multiline for readability:

--- rigid2d/include/waypoints/waypoints.hpp
+++ rigid2d/include/waypoints/waypoints.hpp
@@ -22,7 +22,9 @@ namespace rigid2d
 		Waypoints() : waypoints(5, std::vector<double>(3,0)), time_it(0), state(0), waypoint_index(0){};
 		
 		Waypoints(Transform2D pose, double angular_vel, double trans_vel, double freq) : 
-		robot(pose), angular_vel(angular_vel), trans_vel(trans_vel), waypoints(5, std::vector<double>(3,0)), time_it(0), state(0), waypoint_index(0), freq(freq){};
+			robot(pose), angular_vel(angular_vel), trans_vel(trans_vel), 
+			waypoints(5, std::vector<double>(3,0)), 
+			time_it(0), state(0), waypoint_index(0), freq(freq){};
 		
 		/// \brief based on current state returns needed body velocity to get to next waypoint
 		/// \return a bdy velocity that gets turtlebot to the next waypoint
