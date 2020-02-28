9f74fc709234d6645143bc42e6aff44b2850e403
JCohner <joshuacohen2019@u.northwestern.edu>
2020-02-27 18:54:29 -0600

    prepping for skeleton

--- nuslam/src/draw_map.cpp
+++ nuslam/src/draw_map.cpp
@@ -1,5 +1,28 @@
 #include <ros/ros.h>
+#include <nuslam/TurtleMap.h>
+#include <visualization_msgs/Marker.h>
+
+ros::Subscriber landmark_sub;
+
+void landmark_sub_callback(nuslam::TurtleMap data)
+{
+
+}
+
+void loop()
+{
+	
+}
+
+
+void setup()
+{
+	ros::NodeHandle nh;
+	landmark_sub = nh.subscribe("landmarks", 1, &landmark_sub_callback);
+}
 
 int main(int argc, char * argv[]){
-	
+	ros::init(argc, argv, "landmarks");
+	setup();
+	loop();
 }
--- nuslam/src/landmarks.cpp
+++ nuslam/src/landmarks.cpp
@@ -1,5 +1,29 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nuslam/TurtleMap.h>
+
+ros::Publisher landmark_pub;
+
+void loop()
+{
+
+	ros::Rate r(60);
+	while(ros::ok())
+	{
+		r,sleep();
+		ros.spinOnce();
+	}
+}
+
+
+void setup()
+{
+	ros::NodeHandle nh;
+	landmark_pub = nh.advertise<nuslam::TurtleMap>("landmarks", 1);
+}
 
 int main(int argc, char * argv[]){
-	
+	ros::init(argc, argv, "landmarks");
+	setup();
+	loop();
 }
