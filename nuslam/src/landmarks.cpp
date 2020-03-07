548b6c7e9d2075dfc14e1ff48789e33a93d3b257
JCohner <joshuacohen2019@u.northwestern.edu>
2020-03-07 14:13:55 -0600

    tuning circle fitting algorithm

--- nuslam/src/landmarks.cpp
+++ nuslam/src/landmarks.cpp
@@ -207,13 +207,22 @@ void laser_sub_callback(sensor_msgs::LaserScan data)
 
 		//make our H matrices
 		Eigen::Matrix4f H, H_inv;
+		H = H.Zero(4,4);
+		H_inv = H_inv.Zero(4,4);
 		H(0,0) = 8 * z_mean;
 		H(0,3) = 2;
 		H(3,0) = 2;
 		H(1,1) = 1;
 		H(2,2) = 1;
-		H_inv = H.transpose();
 
+		H_inv(0,3) = 0.5;
+		H_inv(3,0) = 0.5;
+		H_inv(1,1) = 1;
+		H_inv(2,2) = 1;
+		H_inv(3,3) = -2 * z_mean;
+
+		// ROS_INFO_STREAM(H);
+		// ROS_INFO_STREAM(H_inv);
 
 		//compute SVD of Z
 		Eigen::JacobiSVD<Eigen::Matrix4f> svd(Z,  0x04 | 0x10); //values of ComputeFullU | ComputeFullV
@@ -221,24 +230,29 @@ void laser_sub_callback(sensor_msgs::LaserScan data)
 		auto V = svd.matrixV();
 		auto singVals = svd.singularValues();
 
-		auto sig4 = singVals(3); //make sure grabbing properly
+		double sig4 = singVals(3); //make sure grabbing properly
+		ROS_INFO("sing v 4 is: %f", sig4);
 		Eigen::Vector4f A;
 		Eigen::Matrix4f S = singVals.asDiagonal();
 		if (sig4 < 1e-12)
 		{
-			A = V.col(4);
+			A = V.col(3);
 		} else {
 			Eigen::Matrix4f Y = V * S * V.transpose() ;
-			auto Q = Y * H_inv * Y.transpose();
+			Eigen::Matrix4f Q = Y * H_inv * Y;
 			Eigen::SelfAdjointEigenSolver<Eigen::Matrix4f> es(Q);
-			auto eig_vals = es.eigenvalues();
-			double min_val = eig_vals.minCoeff();
-			int min_idx;
+			Eigen::Vector4f eig_vals = es.eigenvalues();
+
+			double min_val(1e10), check_val;// = eig_vals.minCoeff(); //need to find min postiive
+			int min_idx = 0;
 			for (int i = 0; i < 4; i++)
 			{
-				if (eig_vals(i) == min_val) 
+				check_val = eig_vals(i);
+				ROS_INFO("check val is: %f", check_val);
+				if (check_val < min_val && check_val > 0) 
 				{
 					min_idx = i;
+					min_val = check_val;
 				}
 			}			
 
@@ -250,13 +264,12 @@ void laser_sub_callback(sensor_msgs::LaserScan data)
 		
 		double a = -A(1)/(2 * A(0));
 		double b = -A(2)/(2 * A(0));
-		double R_squared = (std::pow(A(1),2) + std::pow(A(2),2) - 4 * A(0) * A(3))/(4 * std::pow(A(1),2));
+		double R_squared = (std::pow(A(1),2) + std::pow(A(2),2) - 4 * A(0) * A(3))/(4 * std::pow(A(0),2));
 		double R = std::sqrt(R_squared);
 		pt center(x_cent + a, y_cent + b);
 		Circle circ(center, R);
 
 		//TODO: find root mean square error
-		ROS_INFO("fuck the police");
 		nuslam::TurtleMap msg;
 		msg.centerX = circ.center.x;
 		msg.centerY = circ.center.y;
